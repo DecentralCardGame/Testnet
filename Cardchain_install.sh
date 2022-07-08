73c32866bfdf7f3ad85df21739611bdfaf3914df
valNodeOne <nodeone@_HOSTNAME_>
2022-07-08 16:49:23 +0200

    inital commit

---
+++ Cardchain_install.sh
@@ -0,0 +1,77 @@
+#!/bin/bash -i
+set -e
+
+echo -n "Enter your validator name: "
+read NODE_MONIKER
+CHAIN_ID=Cardchain
+GENESIS_URL=""
+
+PEERS="9ac1f7c37ccabe74e7d52cb0b7685972bffc63e8@45.136.28.158:26658"
+
+echo  "Downloading Binary..."
+curl https://get.ignite.com/DecentralCardGame/Cardchain@latest! | sudo bash
+
+
+echo  "Initializing Cardchain..."
+Cardchain config chain-id $CHAIN_ID
+Cardchain init $NODE_MONIKER --chain-id $CHAIN_ID
+
+echo  "Getting Genesis file..."
+cp $HOME/Testnet1/genesis.json $HOME/.Cardchain/config/genesis.json
+
+
+echo "Seting persistent peers..."
+sed -i -e "/persistent_peers =/ s/= .*/= \"$PEERS\"/"  $HOME/.Cardchain/config/config.toml
+
+Cardchain unsafe-reset-all
+
+echo "Creating Service..."
+sudo tee <<EOF >/dev/null /etc/systemd/system/Cardchaind.service
+[Unit]
+Description=Cardchain Daemon
+After=network-online.target
+[Service]
+User=$USER
+ExecStart=$(which Cardchain) start
+Restart=always
+RestartSec=10
+LimitNOFILE=65535
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo systemctl daemon-reload
+sudo systemctl enable Cardchaind
+sudo systemctl restart Cardchaind
+
+while [ "$(curl -s localhost:26657/status | jq ".result.sync_info.catching_up")" != "false" ]
+do
+	echo "Catching up. Sleeping for 1 minute."
+	sleep 60
+done
+
+echo "Synchonized."
+
+echo "Creating wallet..."
+Cardchain config keyring-backend test
+(Cardchain keys add validator) 2>&1 | tee 
+
+echo "Getting coins from faucet..."
+KEY=$(Cardchain keys show validator --output=json | jq .address -r)
+curl -X POST https://cardchain.crowdcontrol.network/faucet/ -d "{\"address\": \"$KEY\"}"
+
+sleep 60
+
+echo "Create Validator..."
+Cardchain tx staking create-validator \
+  --from=validator \
+  --amount=1000000ubpf \
+  --moniker=$VAL_NAME \
+  --chain-id=$CHAIN_ID \
+  --commission-rate=0.1 \
+  --commission-max-rate=0.5 \
+  --commission-max-change-rate=0.1 \
+  --min-self-delegation=1 \
+  --pubkey=$(Cardchain tendermint show-validator) \
+  --details="hello world" \
+  --yes
