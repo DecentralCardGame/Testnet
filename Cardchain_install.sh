88474fd2df6c8eaaa0c6ddec97d842e01372192e
valNodeOne <81831703+valNodeOne@users.noreply.github.com>
2025-02-22 16:30:41 +0100

    Update Cardchain_install.sh

--- Cardchain_install.sh
+++ Cardchain_install.sh
@@ -11,7 +11,7 @@ CHAIN_BINARY_URL='https://github.com/DecentralCardGame/Cardchain/releases/downlo
 CHAIN_BINARY='cardchaind'
 GENESIS_URL='https://cardchain.crowdcontrol.network/files/genesis.json'
 SEEDS=""
-PEERS="4c18cce48101390741704397f5a58b9979ca742b@202.61.243.56:32056"
+PEERS="4c18cce48101390741704397f5a58b9979ca742b@152.53.103.89:32056"
 
 # Install go 1.21.5
 # echo "Installing go..."
